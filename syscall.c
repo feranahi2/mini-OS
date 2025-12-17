dbd94211ecef2c8e7153cda339617bf8a0491b4b
feranahi2 <156111357+feranahi2@users.noreply.github.com>
2025-12-17 04:04:55 -0500

    Correccion contador de syscalls

--- syscall.c
+++ syscall.c
@@ -311,7 +311,7 @@ syscall(void)
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     // Incrementar el contador de esta syscall
-    if(num < 24)
+    if(num < 26)
       syscall_counts[num]++;
 
     // Si esta activo el trace se mostrara el syscall
